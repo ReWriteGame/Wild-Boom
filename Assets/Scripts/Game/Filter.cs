af2b3815e91f8ab02718a24801f8e8f24968ad46
ReWriteGame <mr.ekstrimal1432@gmail.com>
2021-11-29 23:06:13 +0200

    init commit

---
+++ Assets/Scripts/Game/Filter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Filter : MonoBehaviour
+{
+    [SerializeField]private float forceImpulse = 1;
+    [SerializeField]private Vector2 dirictionMove;
+
+    public UnityEvent addImpulseEvent;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void AddImpulse()
+    {
+        rb.AddForce(dirictionMove.normalized * forceImpulse, ForceMode2D.Impulse);
+        addImpulseEvent?.Invoke();
+    }
+
+    public void AddForce()
+    {
+        rb.AddForce(dirictionMove.normalized * forceImpulse);
+    }
+}
